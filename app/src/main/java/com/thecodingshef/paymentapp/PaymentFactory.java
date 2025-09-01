638a81296c02c2a63b3c967273de15a8c5d7da4e
azimsiddiqui <thecodingshef@gmail.com>
2025-09-02 00:07:28 +0530

    Remove unwanted files and apply .gitignore

--- app/src/main/java/com/thecodingshef/paymentapp/PaymentFactory.java
+++
@@ -1,8 +0,0 @@
-package com.thecodingshef.paymentapp;
-
-public class PaymentFactory {
-    public Payment create(PaymentType type, String amountStr, String provider, String transactionRef) {
-        double amount = Double.parseDouble(amountStr.trim());
-        return new Payment(type, amount, provider, transactionRef);
-    }
-}
